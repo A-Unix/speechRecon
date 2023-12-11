8f99d06a19f15a6c58bce383b3a51b7978b8d4c7
Atul93 <54507991+Atuls-git@users.noreply.github.com>
2023-12-11 22:19:10 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+echo -e "\nUpdating your system, please wait\n"
+sleep 1
+
+# Update system
+apt update
+
 # Install pip
 apt install python3-pip -y
 
+echo -e "\nInstalling required dependencies, please wait!\n"
+sleep 1
+
 # Install required dependencies
 pip install SpeechRecognition
+
+echo -e "\nDone, run the 'main.py' file now!\n"
+sleep 1
